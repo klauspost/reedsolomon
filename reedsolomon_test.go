bb7bd0036ac1cf9b6d1edd5c5b4f2fdd09771182
lukechampine <luke.champine@gmail.com>
2015-08-06 23:37:51 -0400

    fully test Split/Join functions

--- reedsolomon_test.go
+++ reedsolomon_test.go
@@ -8,6 +8,7 @@
 package reedsolomon
 
 import (
+	"bytes"
 	"fmt"
 	"math/rand"
 	"testing"
@@ -546,11 +547,21 @@ func TestEncoderReconstruct(t *testing.T) {
 		t.Fatal("not ok:", ok, "err:", err)
 	}
 
-	// Delete a shard
+	// Recover original bytes
+	buf := new(bytes.Buffer)
+	err = enc.Join(buf, shards, len(data))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !bytes.Equal(buf.Bytes(), data) {
+		t.Fatal("recovered bytes do not match")
+	}
+
+	// Corrupt a shard
 	shards[0] = nil
 	shards[1][0], shards[1][500] = 75, 75
 
-	// Should reconstruct
+	// Should reconstruct (but with corrupted data)
 	err = enc.Reconstruct(shards)
 	if err != nil {
 		t.Fatal(err)
@@ -561,6 +572,52 @@ func TestEncoderReconstruct(t *testing.T) {
 	if ok || err != nil {
 		t.Fatal("error or ok:", ok, "err:", err)
 	}
+
+	// Recovered data should not match original
+	buf.Reset()
+	err = enc.Join(buf, shards, len(data))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if bytes.Equal(buf.Bytes(), data) {
+		t.Fatal("corrupted data matches original")
+	}
+}
+
+func TestSplitJoin(t *testing.T) {
+	var data = make([]byte, 250000)
+	fillRandom(data)
+
+	enc, _ := New(5, 3)
+	shards, err := enc.Split(data)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = enc.Split([]byte{})
+	if err != ErrShortData {
+		t.Errorf("expected %v, got %v", ErrShortData, err)
+	}
+
+	buf := new(bytes.Buffer)
+	err = enc.Join(buf, shards, 50)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !bytes.Equal(buf.Bytes(), data[:50]) {
+		t.Fatal("recovered data does match original")
+	}
+
+	err = enc.Join(buf, [][]byte{}, 0)
+	if err != ErrTooFewShards {
+		t.Errorf("expected %v, got %v", ErrTooFewShards, err)
+	}
+
+	err = enc.Join(buf, shards, len(data)+1)
+	if err != ErrShortData {
+		t.Errorf("expected %v, got %v", ErrShortData, err)
+	}
+
 }
 
 func TestAllMatrices(t *testing.T) {
