add7662759d355f29583ccc725416df40ccc0ba4
frankw <fwessels@xs4all.nl>
2016-07-06 17:37:04 +0200

    Prevent line wrapping using LONG and WORD operands

--- galois_amd64.s
+++ galois_amd64.s
@@ -142,11 +142,9 @@ TEXT ·galMulAVX2(SB), 7, $0
 	MOVOU (DX), X7          // X7: high
 	MOVQ  in_len+56(FP), R9 // R9: len(in)
 
-    BYTE $0xc4; BYTE $0xe3; BYTE $0x4d; BYTE $0x38; BYTE $0xf6 // VINSERTI128 YMM6, YMM6, XMM6, 1 ; low
-                BYTE $0x01
-    BYTE $0xc4; BYTE $0xe3; BYTE $0x45; BYTE $0x38; BYTE $0xff // VINSERTI128 YMM7, YMM7, XMM7, 1 ; high
-                BYTE $0x01
-    BYTE $0xc4; BYTE $0x62; BYTE $0x7d; BYTE $0x78; BYTE $0xc5 // VPBROADCASTB YMM8, XMM5         ; X8: lomask (unpacked)
+	LONG $0x384de3c4; WORD $0x01f6 // VINSERTI128 YMM6, YMM6, XMM6, 1 ; low
+	LONG $0x3845e3c4; WORD $0x01ff // VINSERTI128 YMM7, YMM7, XMM7, 1 ; high
+	LONG $0x787d62c4; BYTE $0xc5   // VPBROADCASTB YMM8, XMM5         ; X8: lomask (unpacked)
 
 	SHRQ  $5, R9         // len(in) /32
 	MOVQ  out+72(FP), DX // DX: &out
@@ -155,14 +153,14 @@ TEXT ·galMulAVX2(SB), 7, $0
 	JZ    done_avx2
 
 loopback_avx2:
-    BYTE $0xc5; BYTE $0xfe; BYTE $0x6f; BYTE $0x06             // VMOVDQU YMM0, [rsi]
-    BYTE $0xc5; BYTE $0xf5; BYTE $0x73; BYTE $0xd0; BYTE $0x04 // VPSRLQ  YMM1, YMM0, 4   ; X1: high input
-    BYTE $0xc4; BYTE $0xc1; BYTE $0x7d; BYTE $0xdb; BYTE $0xc0 // VPAND   YMM0, YMM0, YMM8      ; X0: low input
-    BYTE $0xc4; BYTE $0xc1; BYTE $0x75; BYTE $0xdb; BYTE $0xc8 // VPAND   YMM1, YMM1, YMM8      ; X1: high input
-    BYTE $0xc4; BYTE $0xe2; BYTE $0x4d; BYTE $0x00; BYTE $0xd0 // VPSHUFB  YMM2, YMM6, YMM0   ; X2: mul low part
-    BYTE $0xc4; BYTE $0xe2; BYTE $0x45; BYTE $0x00; BYTE $0xd9 // VPSHUFB  YMM3, YMM7, YMM1   ; X2: mul high part
-    BYTE $0xc5; BYTE $0xed; BYTE $0xef; BYTE $0xe3             // VPXOR   YMM4, YMM2, YMM3    ; X4: Result
-    BYTE $0xc5; BYTE $0xfe; BYTE $0x7f; BYTE $0x22             // VMOVDQU [rdx], YMM4
+	LONG $0x066ffec5             // VMOVDQU YMM0, [rsi]
+	LONG $0xd073f5c5; BYTE $0x04 // VPSRLQ  YMM1, YMM0, 4   ; X1: high input
+	LONG $0xdb7dc1c4; BYTE $0xc0 // VPAND   YMM0, YMM0, YMM8      ; X0: low input
+	LONG $0xdb75c1c4; BYTE $0xc8 // VPAND   YMM1, YMM1, YMM8      ; X1: high input
+	LONG $0x004de2c4; BYTE $0xd0 // VPSHUFB  YMM2, YMM6, YMM0   ; X2: mul low part
+	LONG $0x0045e2c4; BYTE $0xd9 // VPSHUFB  YMM3, YMM7, YMM1   ; X2: mul high part
+	LONG $0xe3efedc5             // VPXOR   YMM4, YMM2, YMM3    ; X4: Result
+	LONG $0x227ffec5             // VMOVDQU [rdx], YMM4
 
 	ADDQ $32, SI       // in+=32
 	ADDQ $32, DX       // out+=32
