67b3e7e04d916f738bb8904228bfa79da8b6f739
liangsongtaohit <liangsongtao@qiniu.com>
2017-02-26 20:31:59 +0800

    [fix] add test case

--- reedsolomon_test.go
+++ reedsolomon_test.go
@@ -287,6 +287,14 @@ func BenchmarkDecode14x10x16_M(b *testing.B) {
 	benchmarkReconstruct(b, 14, 10, 16776168)
 }
 
+func BenchmarkVerify28x4x16_M(b *testing.B) {
+	benchmarkVerify(b, 28, 4, 16776168)
+}
+
+func BenchmarkVerify14x10x16_M(b *testing.B) {
+	benchmarkVerify(b, 14, 10, 16776168)
+}
+
 func BenchmarkEncode10x2x10000(b *testing.B) {
 	benchmarkEncode(b, 10, 2, 10000)
 }
