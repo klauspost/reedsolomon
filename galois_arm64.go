c357082a63dcf18976d4013176d33610077cf8df
frankw <fwessels@xs4all.nl>
2017-08-21 14:15:36 -0700

    Fix ARM code with missing function.

--- galois_arm64.go
+++ galois_arm64.go
@@ -39,3 +39,10 @@ func galMulSliceXor(c byte, in, out []byte, ssse3, avx2 bool) {
 		}
 	}
 }
+
+// slice galois add
+func sliceXor(in, out []byte, sse2 bool) {
+	for n, input := range in {
+		out[n] ^= input
+	}
+}
