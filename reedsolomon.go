f7a08925adb5b77a30be66a995bb343645a7f273
dssysolyatin <sysolyatin.dima@yandex.ru>
2019-06-25 17:05:52 +0300

    Improve quick check for case when dataOnly is true

--- reedsolomon.go
+++ reedsolomon.go
@@ -666,12 +666,16 @@ func (r reedSolomon) reconstruct(shards [][]byte, dataOnly bool) error {
 	// Quick check: are all of the shards present?  If so, there's
 	// nothing to do.
 	numberPresent := 0
+	dataPresent := 0
 	for i := 0; i < r.Shards; i++ {
 		if len(shards[i]) != 0 {
 			numberPresent++
+			if i < r.DataShards {
+				dataPresent++
+			}
 		}
 	}
-	if numberPresent == r.Shards {
+	if numberPresent == r.Shards || dataOnly && dataPresent == r.DataShards {
 		// Cool.  All of the shards data data.  We don't
 		// need to do anything.
 		return nil
