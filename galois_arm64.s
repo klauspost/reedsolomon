01b307ec91a7a7587226724141df961f3a9b8d26
Frank Wessels <fwessels@xs4all.nl>
2020-05-19 06:03:47 -0700

    Minor refactor for arm NEON version using macros (#147)
    * Minior refactor for arm NEON version using macros

--- galois_arm64.s
+++ galois_arm64.s
@@ -3,6 +3,26 @@
 // Copyright 2015, Klaus Post, see LICENSE for details.
 // Copyright 2017, Minio, Inc.
 
+#define LOAD(LO1, LO2, HI1, HI2) \
+	VLD1.P 32(R1), [LO1.B16, LO2.B16] \
+	                                  \
+	\ // Get low input and high input
+	VUSHR  $4, LO1.B16, HI1.B16       \
+	VUSHR  $4, LO2.B16, HI2.B16       \
+	VAND   V8.B16, LO1.B16, LO1.B16   \
+	VAND   V8.B16, LO2.B16, LO2.B16
+
+#define GALOIS_MUL(MUL_LO, MUL_HI, OUT1, OUT2, TMP1, TMP2) \
+	\ // Mul low part and mul high part
+	VTBL V0.B16, [MUL_LO.B16], OUT1.B16  \
+	VTBL V10.B16, [MUL_HI.B16], OUT2.B16 \
+	VTBL V1.B16, [MUL_LO.B16], TMP1.B16  \
+	VTBL V11.B16, [MUL_HI.B16], TMP2.B16 \
+	                                     \
+	\ // Combine results
+	VEOR OUT2.B16, OUT1.B16, OUT1.B16    \
+	VEOR TMP2.B16, TMP1.B16, OUT2.B16
+
 // func galMulNEON(low, high, in, out []byte)
 TEXT ·galMulNEON(SB), 7, $0
 	MOVD in_base+48(FP), R1
@@ -26,23 +46,8 @@ TEXT ·galMulNEON(SB), 7, $0
 
 loop:
 	// Main loop
-	VLD1.P 32(R1), [V0.B16, V1.B16]
-
-	// Get low input and high input
-	VUSHR $4, V0.B16, V10.B16
-	VUSHR $4, V1.B16, V11.B16
-	VAND  V8.B16, V0.B16, V0.B16
-	VAND  V8.B16, V1.B16, V1.B16
-
-	// Mul low part and mul high part
-	VTBL V0.B16, [V6.B16], V4.B16
-	VTBL V10.B16, [V7.B16], V5.B16
-	VTBL V1.B16, [V6.B16], V14.B16
-	VTBL V11.B16, [V7.B16], V15.B16
-
-	// Combine results
-	VEOR V5.B16, V4.B16, V4.B16
-	VEOR V15.B16, V14.B16, V5.B16
+	LOAD(V0, V1, V10, V11)
+	GALOIS_MUL(V6, V7, V4, V5, V14, V15)
 
 	// Store result
 	VST1.P [V4.D2, V5.D2], 32(R5)
@@ -76,24 +81,11 @@ TEXT ·galMulXorNEON(SB), 7, $0
 
 loopXor:
 	// Main loop
-	VLD1.P 32(R1), [V0.B16, V1.B16]
-	VLD1   (R5), [V20.B16, V21.B16]
+	VLD1 (R5), [V20.B16, V21.B16]
 
-	// Get low input and high input
-	VUSHR $4, V0.B16, V10.B16
-	VUSHR $4, V1.B16, V11.B16
-	VAND  V8.B16, V0.B16, V0.B16
-	VAND  V8.B16, V1.B16, V1.B16
+	LOAD(V0, V1, V10, V11)
+	GALOIS_MUL(V6, V7, V4, V5, V14, V15)
 
-	// Mul low part and mul high part
-	VTBL V0.B16, [V6.B16], V4.B16
-	VTBL V10.B16, [V7.B16], V5.B16
-	VTBL V1.B16, [V6.B16], V14.B16
-	VTBL V11.B16, [V7.B16], V15.B16
-
-	// Combine results
-	VEOR V5.B16, V4.B16, V4.B16
-	VEOR V15.B16, V14.B16, V5.B16
 	VEOR V20.B16, V4.B16, V4.B16
 	VEOR V21.B16, V5.B16, V5.B16
 
